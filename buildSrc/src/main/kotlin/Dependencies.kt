5027f2d27cb885ba10f8236c8b897707364c83bf
Arınç Atlamaz <arincatlamaz@gmail.com>
2024-07-08 22:27:33 +0200

    Added first buildSrc features

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,10 @@
+package kotlin
+
+object Dependencies {
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val okHttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
+
+
+    const val hilt = "com.google.dagger:hilt-android:${Versions.hilt}"
+    const val hiltPlugin = "com.google.dagger.hilt.android"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,7 @@
+package kotlin
+
+object Versions {
+    const val retrofit = "2.11.0"
+    const val okHttp = "4.12.0"
+    const val hilt = "2.49"
+}
